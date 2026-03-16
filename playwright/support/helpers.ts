b55b2e0b6c7d6bc6703f4f71eda0cba5a1d5193b
unknown <wesley.leal@chavesnamao.com.br>
2026-03-16 13:48:39 -0300

    refactor do codigo + segundo desafio:ct03

--- playwright/support/helpers.ts
+++ playwright/support/helpers.ts
@@ -1,13 +1,13 @@
-import { Page } from '@playwright/test'
-
 export function generateOrderCode() {
-    const prefix = "VLO-";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let randomPart = "";
-  
-    for (let i = 0; i < 6; i++) {
-      randomPart += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-  
-    return prefix + randomPart;
+  const prefix = 'VLO'
+
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  let randomPart = ''
+
+  for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length)
+      randomPart += chars[randomIndex]
   }
+
+  return `${prefix}-${randomPart}`
+}
