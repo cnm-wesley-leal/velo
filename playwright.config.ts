678cfe7b7869bc54c7707746c579985fc7facbd8
unknown <wesley.leal@chavesnamao.com.br>
2026-04-30 12:44:18 -0300

    ajuste base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+dotenv.config({ path: path.resolve(process.cwd(), '.env'), quiet: true });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
